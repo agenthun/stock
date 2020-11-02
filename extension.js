43c3a257352098ca96b1c2061881673e3de1257b
henry.hu <henry.hu@cootek.cn>
2020-11-03 01:54:41 +0800

    api

--- extension.js
+++ extension.js
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const axios = require('axios');
+let statusBarItems = {};
+let timer = null;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,31 +12,35 @@ const axios = require('axios');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "stock" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('stock.agent', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from stock!');
-
+		fetchData();
 	});
-
 	context.subscriptions.push(disposable);
 
+	timer = setInterval(fetchData, 1500);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+	timer && clearInterval(timer);
+}
+
+exports.deactivate = deactivate;
+
+module.exports = {
+	activate,
+	deactivate
+}
+
+function fetchData() {
 	const stockCode = '603893';
 	const thsBasedHost = `http://d.10jqka.com.cn/`;
 	const thsStockAll = `${thsBasedHost}v6/line/hs_${stockCode}/01/all.js`;
 	axios.default
 		.get(thsStockAll
 			, {
-				timeout: 1000,
+				timeout: 5000,
 				headers: {
 					'Referer': 'http://m.10jqka.com.cn/stockpage/',
 					'Accept': 'application/json, text/plain, */*',
@@ -43,27 +49,34 @@ function activate(context) {
 			}
 		)
 		.then(resp => {
-			var s = resp.data.replace('quotebridge_v6_line_hs_603893_01_all(', '');
-			console.log(`${s}`);
-			createStatusBarItem(resp.data);
+			try {
+				const json = resp.data.replace(/^quotebridge(.*\()/g, '').slice(0, -1);
+				processStockResp(stockCode, json);
+			} catch (error) {
+				console.error(error);
+			}
 		})
 		.catch(e => {
 			console.error(e);
 		})
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() { }
-
-module.exports = {
-	activate,
-	deactivate
+function processStockResp(stockCode, json) {
+	const text = showStockData(json);
+	if (statusBarItems[stockCode]) {
+		statusBarItems[stockCode].text = text;
+	} else {
+		statusBarItems[stockCode] = createStatusBarItem(text);
+	}
 }
 
-function createStatusBarItem(item) {
+function showStockData(data) {
+	return data;
+}
+
+function createStatusBarItem(text) {
 	const barItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
-	barItem.text = `${item}`;
+	barItem.text = `${text}`;
 	barItem.show();
 	return barItem;
 }
